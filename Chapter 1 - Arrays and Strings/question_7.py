1c8bd5ddc7e830b235bd77fa4536c5e483c5eb1f
Dylan Harness <dharness@uwo.ca>
2016-08-25 20:33:12 -0400

    q7

--- Chapter 1 - Arrays and Strings/question_7.py
+++ Chapter 1 - Arrays and Strings/question_7.py
@@ -1,10 +1,20 @@
 # Write an algorithm such that if an element in an MxN matrix is 0, its entire row
 # and column are set to 0.
 
-def zero_out_row(m):
-  for i, row in enumerate(m):
-    if (0 in row):
-      m[i] = [0] * len(row)
+def minesweeper(m):
+  mines = {'xs': [], 'ys': []}
+  for y, row in enumerate(m):
+    for x, el in enumerate(row):
+      if(el == 0):
+        mines['xs'].append(x)
+        mines['ys'].append(y)
+
+  for y, row in enumerate(m):
+    for x, el in enumerate(row):
+      if (y in mines['ys']):
+        m[y][x] = 0
+      elif (x in mines['xs']):
+        m[y][x] = 0
   return m
 
 matrix = [
@@ -17,7 +27,7 @@ matrix = [
   [1,2,3,4,5]
 ]
 
-r1 = zero_out_row(matrix)
+r1 = minesweeper(matrix)
 
 for row in r1:
   print(row)
