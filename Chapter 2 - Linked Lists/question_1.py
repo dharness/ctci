53ffecc0385d208bf7aaa9fed9326d0397798ffb
Dylan Harness <dharness@uwo.ca>
2016-08-28 01:37:11 -0400

    added all the rums

--- Chapter 2 - Linked Lists/question_1.py
+++ Chapter 2 - Linked Lists/question_1.py
@@ -21,19 +21,32 @@ def remove_dupes(list):
 def remove_dupes2(list):
   """ Removes all duplicate data without outside data structures"""
   n = list.head
-  if n:
-    n2 = n.next_node
   while n:
-    while n2:
-      print(n.data, n2.data)
-      n2 = n2.next_node
+    if n:
+      n2 = n.next_node
+      prev2 = n
+      while n2:
+        if n.data == n2.data:
+          prev2.next_node = n2.next_node
+        else:
+          prev = n2
+        n2 = n2.next_node
     n = n.next_node
+  return list
 
 my_list = LinkedList()
-my_list.append_left('Rumm')
-my_list.append_left('Rumm')
-my_list.append_left('Rumm')
-my_list.append_left('Rumm')
-my_list.append_right('Rumm')
-my_list = remove_dupes(my_list)
+my_list.append_left('Rumm1')
+my_list.append_left('Rumm2')
+my_list.append_left('Rumm3')
+my_list.append_left('Rumm4')
+my_list.append_left('Rumm4')
+my_list.append_left('Rumm4')
+my_list.append_left('Rumm4')
+my_list.append_left('Rumm4')
+my_list.append_left('Rumm4')
+my_list.append_left('Rumm4')
+my_list.append_left('Rumm4')
+my_list.append_left('Rumm4')
+print(my_list)
+my_list = remove_dupes2(my_list)
 print(my_list)
